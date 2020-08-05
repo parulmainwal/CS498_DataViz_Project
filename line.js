de8be7a3667cc6012f4e700ad584482729b668a9
parulmainwal <parulmainwal@gmail.com>
2020-08-05 17:05:12 -0500

    Update line.js

--- line.js
+++ line.js
@@ -10,8 +10,8 @@ function makeLineChart(dataset, xName, yObjs, axisLables) {
 
     chartObj.data = dataset;
     chartObj.margin = {top: 15, right: 60, bottom: 30, left: 50};
-    chartObj.width = 650 - chartObj.margin.left - chartObj.margin.right;
-    chartObj.height = 480 - chartObj.margin.top - chartObj.margin.bottom;
+    chartObj.width = 550 - chartObj.margin.left - chartObj.margin.right;
+    chartObj.height = 380 - chartObj.margin.top - chartObj.margin.bottom;
 
 // So we can pass the x and y as strings when creating the function
     chartObj.xFunct = function(d){return d[xName]};
